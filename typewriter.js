043cd6ec0d0078c0f78ead5134388ba32fb70764
dbeattie <darren@fettle.ca>
2019-12-03 17:34:25 +0000

    made some minor changes and played with the code a bit

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,5 @@
-const sentence = "hello there from lighthouse labs\n";
+let userName = `Darren Beattie`
+const sentence = `Hello there from ${userName} & Lighthouse Labs!\n`;
 let delay = 0
 for (const char of sentence) {
   setTimeout(() => {
